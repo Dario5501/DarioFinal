f6af11232d79799c4ae018e4bb1fa6b04e001bcd
Dario5501 <dariox@outlook.com>
2021-06-26 21:22:20 +0200

    classes zijn aangemaakt

---
+++ src/Aftand.java
@@ -0,0 +1,7 @@
+public class Aftand {
+
+// deze methode verteld hoe ver een wc is aan de hand van gegeven km.
+    public String getAfstand(double afstand) {
+        return "";
+    }
+}
---
+++ src/Prijs.java
@@ -0,0 +1,10 @@
+public class Prijs {
+
+    public Prijs(){
+
+    }
+    // berekent prijs dat een klant moet betalen.
+    public double PrijsBerekenen(boolean contant, boolean invalide, boolean pin, int afstand) {
+        return 0;
+    }
+}
